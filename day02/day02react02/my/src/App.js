86b523818ee3039732216e5ccff638cec55676ec
minji <minprr1029@gmail.com>
2024-08-13 16:36:05 +0900

    day02

--- day02/day02react02/my/src/App.js
+++ day02/day02react02/my/src/App.js
@@ -1,57 +1,36 @@
+import React from 'react';
 import "./App.css";
+import Input from "./components/Input";
+import Output from "./components/Output";
 
 const App = ()=>{
+    // useState() 훅을 이용해서 state 생성
+    // Input 컴포넌트에서 데이터를 추가 하고
+    // Ouput 컴포넌트에서 데이터를 접근 할 수 있다.
+    const [todoListArr, setTodoList] = React.useState([
+        {no:101, title:'운동하기', done:false},
+        {no:102, title:'운동하기2', done:false},
+        {no:103, title:'운동하기3', done:false}
+    ]);
+    const [noCnt, setNoCnt] = React.useState(104);
+
+    function appendItem(title) {
+        // setTodoList 함수를 이용해서 데이터 갱신
+        // 스프리드 연산자 사용하면 편리함.
+        const newItem = {no:noCnt, title:title, done:false};
+        setNoCnt(noCnt+1);
+        setTodoList([...todoListArr, newItem]);
+    }
+
     return (<div>
         <header className="jumbotron">
             <h1>Todo List</h1>
+            <p>오늘 할 일을 입력 하세요</p>
         </header>
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Something clever.." />
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="button">OK</button>
-                <button class="btn btn-danger" type="button">Cancel</button>
-            </div>
-        </div>
-        <div>
-            <table class="table table-hover">
-                <thead>
-                    <tr style={{textAlign:"center"}}>
-                        <th>Confirm</th>
-                        <th>Titme</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <input type="checkbox"></input>
-                        </td>
-                        <td>미니프로젝트 만들기</td>
-                        <td>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="checkbox"></input>
-                        </td>
-                        <td>조별 스터디 자료 준비</td>
-                        <td>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="checkbox"></input>
-                        </td>
-                        <td>마이 홈페이지 만들기</td>
-                        <td>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        {/* 입력 기능 */}
+        <Input appendItem={appendItem} />
+        {/* 목록 출력 기능 - 추가된 속성은 props로 전달. */}
+        <Output todoListArr={todoListArr} />
     </div>);
 };
 
--- day02/day02react02/my/src/components/Output.js
+++ day02/day02react02/my/src/components/Output.js
@@ -1,24 +1,27 @@
-export default () => {
-    const todoListArr = [<tr>
-        <td colSpan={3}>미니프로젝트 만들기1</td>
-    </tr>,<tr>
-        <td colSpan={3}>미니프로젝트 만들기2</td>
-    </tr>,<tr>
-        <td colSpan={3}>미니프로젝트 만들기3</td>
-    </tr>,<tr>
-        <td colSpan={3}>미니프로젝트 만들기</td>
-    </tr>];
+export default (props) => {
+    let todoListArr = props.todoListArr;
     return (<div>
-        <table id="todoListTbl" class="table table-hover">
+        <table id="todoListTbl" className="table table-hover">
             <thead>
                 <tr style={{textAlign:"center"}}>
                     <th>Confirm</th>
-                    <th>Titme</th>
+                    <th>Title</th>
                     <th>Delete</th>
                 </tr>
             </thead>
             <tbody>
-                {todoListArr}
+                {todoListArr.map((item, idx)=>{
+                    // 중괄호 enter하면 return문을 꼭 넣어야 한다.
+                    return (<tr key={item.no}>
+                        <td>
+                            <input type="checkbox"></input>
+                        </td>
+                        <td>{item.title}</td>
+                        <td>
+                            <button className="btn btn-danger" type="button">Delete</button>
+                        </td>
+                    </tr>);
+                })}
             </tbody>
         </table>
     </div>);
