c6da0ffcb11d65d87a3f02aca961df25782c57c8
minji <minprr1029@gmail.com>
2024-08-19 10:49:15 +0900

    day04

---
+++ day04/todolist/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+
+//export default ()=>{
+
+//}
+
+const App = ()=>{
+    return (<div className="App-header">
+        <h1>App</h1>
+    </div>)
+}
+export default App
---
+++ day04/todolist/src/index.js
@@ -0,0 +1,9 @@
+import ReactDom from "react-dom/client";
+import react from "react";
+import "./index.css";
+import App from "./App";
+
+const container = document.getElementById("root")
+const root = ReactDom.createRoot(container);
+root.render(<h2>Todo List App</h2>);
+root.render(<App />);
