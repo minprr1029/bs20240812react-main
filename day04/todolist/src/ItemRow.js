f80cdebdedad6b626e1141fc29915feb2c3c661a
minji <minprr1029@gmail.com>
2024-08-20 09:22:03 +0900

    day04

--- day04/todolist/src/ItemRow.js
+++ day04/todolist/src/ItemRow.js
@@ -4,6 +4,7 @@ const ItemRow = ({item, onDoneFlag, onDelete, onEdit})=>{
     const [flag, setFlag] = useState(false);
     const [outputTitle, setOutputTtile] = useState(item.title);
     const lineThroughClass = {textDecoration:"line-through", color:"blue"}
+    const [titleTmp, setTitleTmp] = useState(item.title);
     return(<div className="input-group mb-3">
         <div className="input-group-prepend">
           <div className="input-group-text">
@@ -19,18 +20,23 @@ const ItemRow = ({item, onDoneFlag, onDelete, onEdit})=>{
               value={outputTitle}
               onChange={(e)=>{
                   setOutputTtile(e.target.value);
+                  setTitleTmp(e.target.value);
               }}
               onFocus={(e)=>{
                   setFlag(true);
               }}
               onBlur={(e)=>{
-                console.log("blur");
                 setFlag(false);
+                // 수정 하다가 말았기 때문에 원복한다.
+                setOutputTtile(item.title);
               }}
               />
           <div className="input-group-append">
               <button onClick={()=>{
-                onEdit({no:item.no, title:outputTitle, done:item.done});
+                // edit 버튼을 클릭하면 blur가 먼저 호출...
+                console.log("edit ...:", titleTmp);
+                setOutputTtile(titleTmp);
+                onEdit({no:item.no, title:titleTmp, done:item.done});
               }} className="btn btn-primary" type="button">Edit</button>  
               <button onClick={()=>{
                   onDelete(item);
